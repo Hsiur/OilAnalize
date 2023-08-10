2da11ac9277da96cebb45e7142777538b4f21a81
Hsiur <ruish10@me.com>
2023-08-10 12:45:42 +0500

    Initial Commit

---
+++ OilAnalize/OilAnalizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OilAnalizeApp.swift
+//  OilAnalize
+//
+//  Created by Руслан Ишмухаметов on 10.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct OilAnalizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
